6770ae0052a165c9fe7b55a3fb06739555d22616
sharath9620 <s.p@castsoftware.com>
2023-06-30 15:41:41 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,48 +1,55 @@
-import os
-import win32com.client as win32
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import smtplib
+import ssl
 
 def send_email():
+        
+    # Define these once; use them twice!
+    strFrom = 's.p@castsoftware.com'
+    strTo = 's.p@castsoftware.com,b.prakash@castsoftware.com,s.mahton@castsoftware.com'
+    HOST = 'email-smtp.eu-central-1.amazonaws.com'
+    PORT = 587
+    USERNAME_SMTP = 'AKIAZV4FQR23NGDUIIOI'
+    PASSWORD_SMTP = 'BNcNAA8oGY2cl2VRwufb8hgRyAScUVxc0S6e8c1Z+ewP'
 
-    # construct Outlook application instance
-    olApp = win32.Dispatch('Outlook.Application')
-    olNS = olApp.GetNameSpace('MAPI')
 
-    # construct the email item object
-    mailItem = olApp.CreateItem(0)
-    mailItem.Subject = 'Application Health File'
-    mailItem.BodyFormat = 1
-    mailItem.To = 's.p@castsoftware.com; m.sharma@castsoftware.com; n.kaplan@castsoftware.com'
+    # Create the root message and fill in the from, to, and subject headers
+    msgRoot = MIMEMultipart('related')
+    msgRoot['Subject'] = 'test message'
+    msgRoot['From'] = strFrom
+    msgRoot['To'] = strTo
+    msgRoot.preamble = 'This is a multi-part message in MIME format.'
 
-    attachment = mailItem.Attachments.Add(os.path.join(os.getcwd(), 'img/logo.png'))
-    attachment.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001F", "MyId1")
+    # Encapsulate the plain and HTML versions of the message body in an
+    # 'alternative' part, so message agents can decide which they want to display.
+    msgAlternative = MIMEMultipart('alternative')
+    msgRoot.attach(msgAlternative)
+
+    # This example assumes the image is in the current directory
+    with open('img\logo.png', 'rb') as fp:
+        msgImage = MIMEImage(fp.read())
+    
+    # Define the image's ID as referenced above
+    msgImage.add_header('Content-ID', '<image1>')
+    msgRoot.attach(msgImage)
 
     with open('ApplicationHealth.htm', 'r') as myfile:
         data=myfile.read()
 
-    mailItem.HTMLBody = "<html> <body lang=EN-US style='word-wrap:break-word'> <div class=WordSection1> <table border=0 cellspacing=0 cellpadding=0 width='100%'> <tr> <td width='25%' valign=top> " + "<img src=""cid:MyId1"">" + data 
+    msgHTML = MIMEText(data, 'html')
+    msgAlternative.attach(msgHTML)
 
-    mailItem.Display()
+    # Try to send the message.
+    try:
+        context = ssl.create_default_context()
+        with smtplib.SMTP(HOST, PORT) as server:
+            server.starttls(context=context)
+            server.login(USERNAME_SMTP, PASSWORD_SMTP)
+            server.sendmail(strFrom, strTo, msgRoot.as_string())
+            server.close()
+            print("Email sent!")
 
-    mailItem.Save()
-    mailItem.Send()
-
-
-# import os
-# from sendgrid import SendGridAPIClient
-# from sendgrid.helpers.mail import Mail
-
-# def send_email():
-
-#     with open('ApplicationHealth.htm', 'r') as myfile:
-#         data=myfile.read()
-
-#     message = Mail(
-#         from_email='s.p@castsoftware.com',
-#         to_emails='s.p@castsoftware.com',
-#         subject='Application Health File',
-#         html_content="<html> <body lang=EN-US style='word-wrap:break-word'> <div class=WordSection1> <table border=0 cellspacing=0 cellpadding=0 width='100%'> <tr> <td width='25%' valign=top> " + "<img src=""cid:MyId1"">" + data )
-
-#     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-#     response = sg.send(message)
-#     print(response.status_code, response.body, response.headers)
-
+    except smtplib.SMTPException as e:
+        print("Error: ", e)
